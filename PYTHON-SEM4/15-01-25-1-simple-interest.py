ec8addab9dba8dd27bc57fd44acb3bc96b5e8715
sammisam8888 <samuelpriyatam@gmail.com>
2025-01-15 13:01:51 +0530

    15th-January-classwork-updated

---
+++ PYTHON-SEM4/15-01-25-1-simple-interest.py
@@ -0,0 +1,11 @@
+#WAP TO PRINT THE SIMPLE INTEREST BY TAKING USER INPUT
+
+p=float(input("Enter the Principal amount :"))
+r=float(input("Enter the rate of interest :"))
+t=float(input("Enter the time period (in years) :"))
+
+si=(p*r*t)/100
+ci=p*pow((1+r/100),t)
+ci-=p
+print("The simple interest on the given amount is :",si)
+print("The compound interest on the given amount is :",ci)
