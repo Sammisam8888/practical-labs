aedc2eddeb8b92bc3ec022817b9cade51352b160
sammisam8888 <samuelpriyatam@gmail.com>
2025-02-13 10:51:23 +0530

    completed-daa-13th-feb

--- DAA-SEM4/13-02-25-sorting-comparision.cpp
+++ DAA-SEM4/13-02-25-sorting-comparision.cpp
@@ -1,94 +1,193 @@
-#include<iostream>
+#include <iostream>
 #include <ctime>
 using namespace std;
 
-class Sort {
-    int *arr,n;
-    int arrsize[4]={5,10,15,20};
-	double timediff[48];
-	string sortingtype[4]={"Selection Sort ","Insertion Sort ","Bubble Sort ","Heap Sort "};
-	void SelectionSort(){		
-	}
-	void InsertionSort(){
-	}
-	void BubbleSort(){
-	}
-    void HeapSort(){
+class Sort
+{
+    int *arr, n,*temparr;
+    int arrsize[4] = {5, 10, 15, 20};
+    double timediff[16];
+    double mintime = 1e9, maxtime = 0;
+    string sortingtype[4] = {"Selection Sort ", "Insertion Sort ", "Bubble Sort ", "Heap Sort "};
+    string mintype, maxtype;
+
+    void selectionsort()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int minindex = i;
+            for (int j = i; j < n; j++)
+            {
+                if (arr[j] < arr[minindex])
+                {
+                    minindex = j;
+                }
+            }
+            swap(arr[i], arr[minindex]);
+        }
     }
 
-    void display(string message = "") {
-        if (!message.empty()) {
+    void insertionsort()
+    {
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i], j = i;
+            while (j > 0 && arr[j - 1] > key)
+            {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = key;
+        }
+    }
+
+    void bubblesort()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+    }
+
+    void heapsort()
+    {
+        for (int i = n / 2 - 1; i >= 0; i--)
+        {
+            heapify(i, n);
+        }
+        for (int i = n - 1; i > 0; i--)
+        {
+            swap(arr[0], arr[i]);
+            heapify(0, i);
+        }
+    }
+
+    void heapify(int i, int sz)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < sz && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < sz && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            swap(arr[i], arr[largest]);
+            heapify(largest, sz);
+        }
+    }
+
+    void display(string message = "")
+    {
+        if (!message.empty())
+        {
             cout << message << endl;
         }
         cout << "The elements of the array are: ";
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cout << arr[i] << " ";
         }
         cout << endl;
     }
 
-    void getdata() {
-		cout<<"Enter "<<n<<" elements for the array : ";
+    void getdata()
+    {
+        cout << "Enter " << n << " elements for the array : ";
         arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> arr[i];
         }
-	}
+    }
 
-    void comparetime(string minType, double minTime, string maxType, double maxTime) {
-		cout << "The case with the minimum time: " << minType << " took " << minTime << " seconds" << endl;
-        cout << "The case with the maximum time: " << maxType << " took " << maxTime << " seconds" << endl;
-    }	
-
-public:
-    void runsort() {
-        string mintype, maxtype;
-        for (int i = 0; i < 4; i++) {
-        double mintime = 1e9, maxtime = 0;
-            n=arrsize[i];
-            getdata();
-			for (int j=0;j<4;j++){
-            timediff[i*j*3] = double(clock())/CLOCKS_PER_SEC;
-			cout<<"For ";
-			switch(j){
-			case 0:
-				SelectionSort();
-				break;
-			case 1:
-				InsertionSort();
-				break;
-			case 2:
-				BubbleSort();
-				break;
-			case 3:
-				HeapSort();
-				break;
-			}
-			
-			timediff[i*3*j+1]=double(clock()) / CLOCKS_PER_SEC;
-			timediff[i*3*j+2] = timediff[i*3*j+1] - timediff[i*j*3];
-            cout<<"For "<<sortingtype[j]<<endl;
-            cout << "Start Time: " <<  timediff[i*j*3]  << " seconds" << endl;
-            cout << "End Time: " << timediff[i*3*j+1] << " seconds" << endl;
-            cout << "Time taken: " << timediff[i*3*j+2] << " seconds" << endl;
-            if (timediff[i*3*j+2] < mintime) {
-                mintime = timediff[i*3*j+2];
-				mintype= sortingtype[j];
-            }
-            if (timediff[i*3*j+2] > maxtime) {
-                maxtime = timediff[i*3*j+2];
-                maxtype = sortingtype[j];
-            }
-		}
-
-            delete[] arr;
-			comparetime(mintype, mintime, maxtype, maxtime);
+    void comparetime()
+    {
+        cout << "The case with the minimum time: " << mintype << " took " << mintime << " seconds" << endl;
+        cout << "The case with the maximum time: " << maxtype << " took " << maxtime << " seconds" << endl;
+    }
+    void swap(int &a, int &b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    void copyarray(int *newarr, int *oldarr){
+        
+        for (int k = 0; k < n; k++)
+        {
+            newarr[k] = oldarr[k];
         }
+    }
+public:
+    void runsort()
+    {
+        for (int i = 0; i < 4; i++)
+        {   
+            
+            n = arrsize[i];
+            getdata();
+            temparr = new int[n];
+            copyarray(temparr,arr);
+            mintime = 1e9, maxtime = 0;
 
+            for (int j = 0; j < 4; j++)
+            {                
+                int index = i*4+j;
+                double start = double(clock()) / CLOCKS_PER_SEC;
+                if (j == 0)
+                {
+                    selectionsort();
+                }
+                else if (j == 1)
+                {
+                    insertionsort();
+                }
+                else if (j == 2)
+                {
+                    bubblesort();
+                }
+                else if (j == 3)
+                {
+                    heapsort();
+                }
+                double end = double(clock()) / CLOCKS_PER_SEC;
+                timediff[index] = end-start;
+                cout << "For " << sortingtype[j] << endl;
+                cout << "Start Time: " << start << " seconds" << endl;
+                cout << "End Time: " << end << " seconds" << endl;
+                cout << "Time taken: " << timediff[index] << " seconds" << endl;
+                if (timediff[index] < mintime)
+                {
+                    mintime = timediff[index];
+                    mintype = sortingtype[j];
+                }
+                if (timediff[index] > maxtime)
+                {
+                    maxtime = timediff[index];
+                    maxtype = sortingtype[j];
+                }
+                copyarray(arr,temparr);
+            }
+            delete[] temparr;
+            comparetime();
+            delete[] arr;
+        }
     }
 };
 
-int main() {
+int main()
+{
     Sort s;
     s.runsort();
     return 0;
