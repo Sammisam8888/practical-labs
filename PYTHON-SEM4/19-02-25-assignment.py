20b2c10ce0c66576f29a38dba2eef41efaf8aef0
sammisam8888 <samuelpriyatam@gmail.com>
2025-02-19 12:32:03 +0530

    added-assignment

---
+++ PYTHON-SEM4/19-02-25-assignment.py
@@ -0,0 +1,13 @@
+"""
+Q1 - WAP to display the details of te student entities using class and object (roll no, name, branch, regsistration year, cgpa, passout year,  )
+Q2 - WAP to show the details of an employee working in an mnc (ID, NAME, AGE,YEAR OF EXP, CTC, TAX PAYING PER YEAR, COMPANY NAME)
+Q3 - WAP to show the details of the faculty class entities using class and object (faculty id, name, age, teacing subject, years of exp, student improvement percentage per year)
+Q4 - To create a class representing a circle, input methods to calculater its area and perimeter
+Q5 - WAP to create a class representing a shopping bag with methods for adding and removing items and calculating the total price (item name, price)
+Q6 - WAP to demonstrate inheritance of the following problem statement : 
+i - inheritance about latest model of apple iphone
+ii - inheritance about multiple autonomous colleges under a university 
+iii - multiple iits and nits under aicte
+iv - inheritance about netflix membership, and contains a child class condition when one user is watching premium show, live match broadcasting with one device and one screen, no other user can interfere and no screen share is allowed. when other user are trying to login then the account will be freezed for 24 hours
+Q7 - WAP to demonstrate polymorphism about all departments of vssut (number of faculties, students score above 9 cgpa)
+"""
