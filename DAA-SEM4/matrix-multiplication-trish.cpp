ddd0430341d9f9cf8aa6694b74767eab117532af
sammisam8888 <samuelpriyatam@gmail.com>
2025-03-27 10:39:35 +0530

    updated-errors-matrix-multiplication

--- DAA-SEM4/matrix-multiplication-trish.cpp
+++ DAA-SEM4/matrix-multiplication-trish.cpp
@@ -30,16 +30,17 @@ private:
         {
             m[i][i] = 0;
         }
-
+    
         for (int l = 2; l <= n; l++)
         {
             for (int i = 1; i <= n - l + 1; i++)
             {
                 int j = i + l - 1;
                 m[i][j] = INT_MAX;
-                for (int k = i; k <= j - 1; k++)
+    
+                for (int k = i; k < j; k++)
                 {
-                    int q = m[i][k] + m[k + 1][j] + t[i - 1][0] * t[k][1] * t[j][1];
+                    int q = m[i][k] + m[k + 1][j] + t[i][0] * t[k][1] * t[j][1]; // Corrected indexing
                     if (q < m[i][j])
                     {
                         m[i][j] = q;
@@ -49,7 +50,7 @@ private:
             }
         }
     }
-
+    
     void printOptimalParenthesis(int i, int j)
     {
         if (i == j)
