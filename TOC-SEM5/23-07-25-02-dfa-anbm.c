4fd7e3dfaa9c8ae0da28210ff8e3ac642b14dc94
sammisam8888 <samuelpriyatamxd@gmail.com>
2025-07-24 02:44:51 +0530

    completed toc lab 23rd July

---
+++ TOC-SEM5/23-07-25-02-dfa-anbm.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool q1(int n,int m);
+bool q2(int n,int m);
+bool q3(int n,int m);
+
+bool q0(int n,int m){
+	if (n!=0) return q1(--n,m);
+	else if (m!=0) return q3(n,--m);
+	else return 0;
+}
+
+bool q1(int n,int m){
+	if (n!=0) return q0(--n,m);
+	else if (m!=0) return q2(n,--m);
+	else return 0;
+}
+
+bool q2(int n,int m){
+	if (n!=0) return q3(--n,m);
+	else if (m!=0) return q2(n,--m);
+	else return 0;
+}
+
+bool q3(int n,int m){
+	if (n!=0) return q2(--n,m);
+	else if (m!=0) return q3(n,--m);
+	else return 1;
+}
+
+int main(){
+	int n,m;
+	printf("Enter the power of a (a^n) : ");
+	scanf("%d",&n);
+	printf("Enter the power of b (b^m) (m>=1) : ");
+	scanf("%d",&m);
+	
+	printf("The given conditions of : n mod 2=0 & m>=1 over a^%d*b^%d is %s",n,m,(q0(n,m)?"satisfied":"not satisfied"));
+	
+	
+	return 0;
+}
