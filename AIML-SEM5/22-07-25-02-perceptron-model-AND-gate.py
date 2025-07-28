e2489746dd7009b71fe4f9309c0817496e6d189f
sammisam8888 <samuelpriyatam@gmail.com>
2025-07-29 03:38:02 +0530

    rectified

--- AIML-SEM5/22-07-25-02-perceptron-model-AND-gate.py
+++ AIML-SEM5/22-07-25-02-perceptron-model-AND-gate.py
@@ -1,7 +1,7 @@
-wt1, wt2 = -0.5, -0.5
+wt1, wt2 = -0.1, -0.1
 wt0 = -0.4
 x0 = 1
-lr = 0.1
+lr = 0.5
 target = [0, 0, 0, 1]
 inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
 
@@ -10,14 +10,14 @@ def step(n):
 
 def ANDgate(epoch=0):
     global wt0, wt1, wt2
-    total_error = 0
+    totalerror = 0
     print(f"Epoch {epoch + 1}")
     
     for i, (x1, x2) in enumerate(inputs):
         net = x0 * wt0 + x1 * wt1 + x2 * wt2
         out = step(net)
         error = target[i] - out
-        total_error += abs(error)
+        totalerror += abs(error)
 
         wt1 += lr * error * x1
         wt2 += lr * error * x2
@@ -25,7 +25,7 @@ def ANDgate(epoch=0):
 
         print(f"{x1}  {x2}     {target[i]}       {out}      {error}    {round(wt1,2)}    {round(wt2,2)}    {round(wt0,2)}")
 
-    if total_error != 0:
+    if totalerror != 0:
         ANDgate(epoch + 1)
 
 print("Training Perceptron for AND Gate")
